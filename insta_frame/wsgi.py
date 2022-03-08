f32cf8e3b8fd39b2d0a99779770584619ec802e3
Till Hainbach <Till.Hainbach@posteo.de>
2022-03-08 22:23:47 +0100

    wip

--- insta_frame/wsgi.py
+++ insta_frame/wsgi.py
@@ -1,3 +1,12 @@
+import logging
+import os
+import sys
+
 from .__init__ import create_app
 
 application = create_app()
+
+
+if "DYNO" in os.environ:
+    application.logger.addHandler(logging.StreamHandler(sys.stdout))
+    application.logger.setLevel(logging.ERROR)
