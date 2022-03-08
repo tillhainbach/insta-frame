8054581f66d6a8949c986cce33420dca6f52eb79
Till Hainbach <Till.Hainbach@posteo.de>
2022-03-08 21:21:32 +0100

    wip

--- insta_frame/wsgi.py
+++ insta_frame/wsgi.py
@@ -1,11 +1,25 @@
 # from .__init__ import create_app
 
-from flask import Flask
+import shutil
+import tempfile
+
+from flask import Flask, appcontext_tearing_down
 
 # application = create_app()
 
 application = Flask(__name__)
 application.secret_key = "super_secret!"
+
+temp = tempfile.mkdtemp()
+application.config["UPLOAD_FOLDER"] = temp
+
+
+def _cleanup(*args, **kwargs):
+    shutil.rmtree(temp)
+    return
+
+
+appcontext_tearing_down.connect(_cleanup)
 
 
 @application.route("/")
