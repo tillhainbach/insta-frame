9bd60f85decf3d96e370b366b94a7bd671710695
Till Hainbach <Till.Hainbach@posteo.de>
2022-03-08 21:18:07 +0100

    wip

--- insta_frame/wsgi.py
+++ insta_frame/wsgi.py
@@ -1,3 +1,13 @@
-from .__init__ import create_app
+# from .__init__ import create_app
 
-application = create_app()
+from flask import Flask
+
+# application = create_app()
+
+application = Flask(__name__)
+application.secret_key = "super_secret!"
+
+
+@application.route("/")
+def hello_world():
+    return "Hello World!"
